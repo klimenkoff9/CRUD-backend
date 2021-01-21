8e26f86f6a370608a56810e98611ce892ae04a25
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-21 14:18:44 -0500

    added route to get all students

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 module.exports = router;
 
-router.use("/pokemon", require("./pokemon"));
+router.use("/student", require("./student"));
 // router.use('/trainers', require('./trainer'))
 
 router.use((req, res, next) => {
--- api/student.js
+++ api/student.js
@@ -2,3 +2,12 @@ const router = require('express').Router();
 const { Student } = require('../db/models');
 module.exports = router;
 
+router.get("/", async (req, res, next) => {
+    console.log("req.query" , req.query);
+    try {
+        const allStudents = await Student.findAll();
+        res.json(allStudents);
+    } catch (error) {
+        next(error);
+    }
+})
