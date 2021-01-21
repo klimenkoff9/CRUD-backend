b69ab3f90faaf6a2483387e2991313c79d6a5a02
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-21 14:36:24 -0500

    added default values for imageUrl and description attributes of campus model

--- db/models/campuses.js
+++ db/models/campuses.js
@@ -5,35 +5,37 @@
   - [ ] address - not empty or null
   - [ ] description - extremely large text
 */
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = require('../db');
+const db = require("../db");
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = require('../db');
+const db = require("../db");
 
 const Campus = db.define("campus", {
-    name : {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate : {
-            notEmpty: true
-        }
-    },
-    imageUrl : {
-        type: Sequelize.TEXT
-    },
-    address : {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate : {
-            notEmpty: true
-        }
-    },
-    description : {
-        type: Sequelize.TEXT
-    }
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	imageUrl: {
+		type: Sequelize.TEXT,
+		defaultValue: `https://th.bing.com/th/id/Rea604652f50c83d41768a347559e089f?rik=J8rmS2E9pZXdyA&riu=http%3a%2f%2fwww.brooklyn.cuny.edu%2fweb%2fcom_socialImages%2fBrooklynCollegeLibrary_1200x628.jpg&ehk=wkz3LaQJ2HRQ8LgiKSP8e%2bI0Ndl4LFK9InpDBYKn4PI%3d&risl=&pid=ImgRaw`,
+	},
+	address: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	description: {
+        type: Sequelize.TEXT,
+        defaultValue : `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a purus maximus, fermentum erat sed.`
+	},
 });
 
 module.exports = Campus;
