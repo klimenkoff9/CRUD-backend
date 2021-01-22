792568bd28f45ce86a14a167f3d33d36d73a245d
Andrew Previl <drewprevil@gmail.com>
2021-01-21 22:23:59 -0500

    paths to create, find a single and all campuses

--- api/campus.js
+++ api/campus.js
@@ -1,4 +1,45 @@
 const router = require('express').Router();
-const { Campus } = require('../db/models');
-module.exports = router;
 
+const { Campus, Student } = require('../db/models');
+
+//get all campuses path api/campus
+router.get("/", async (req, res, next)=>{
+    try{
+        const table = await Campus.findAll();
+        res.status(200).json(table);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: "no table found"});
+        next(error);
+    }
+})
+//get specific campus with students path api/campus/:id
+router.get("/:id", async(req, res, next)=>{
+    try{
+        const campus = await Campus.findAll({include: Student, where: { id: req.params.id}});
+        res.status(200).json(campus);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: "error finding table"});
+        next(error);
+
+    }
+})
+//post a campus  path api/campus/campus
+router.post("/campus", async (req, res, next)=>{
+    try{
+        const campus = await Campus.create(req.body);
+        res.status(200).json(campus);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: "unable to post campus"});
+        next(error);
+    }
+})
+
+
+
+module.exports = router;
--- db/db.js
+++ db/db.js
@@ -5,7 +5,8 @@ require('dotenv').config(); // requries dotenv
 	process.env.DATABASE_URL || "postgres://localhost:5432/crud" // instead of  crud add the name of your own db 
 ); */
 // for process.env.DATABASE_PASS either out password here it make an .env file and place password there/
-const db = new Sequelize("crud", "yifen", process.env.DATABASE_PASS, {
+
+const db = new Sequelize("campusdb", "postgres", process.env.DATABASE_PASS, {
     host : "localhost",
     dialect: "postgres"
 })
