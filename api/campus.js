d57af38e30b8c3a60a9742e9d0f5048f5a4345e2
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-22 15:47:41 -0500

    added router.put for campus id

--- api/campus.js
+++ api/campus.js
@@ -1,46 +1,66 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { Campus, Student } = require('../db/models');
+const { Campus, Student } = require("../db/models");
 
 //get all campuses path api/campus
-router.get("/", async (req, res, next)=>{
-    try{
-        const table = await Campus.findAll();
-        res.status(200).json(table);
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({message: "no table found"});
-        next(error);
-    }
-})
+router.get("/", async (req, res, next) => {
+	try {
+		const table = await Campus.findAll();
+		res.status(200).json(table);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "no table found" });
+		next(error);
+	}
+});
 //get specific campus with students path api/campus/:CampusNmae
-router.get("/:CampusName", async(req, res, next)=>{
-    try{
-        const campus = await Campus.findAll({include: Student, where: { name: req.params.CampusName}});
-        console.log(campus);
-        res.status(200).json(campus);
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({message: "error finding table"});
-        next(error);
-
-    }
-})
+router.get("/:CampusName", async (req, res, next) => {
+	try {
+		const campus = await Campus.findAll({
+			include: Student,
+			where: { name: req.params.CampusName },
+		});
+		console.log(campus);
+		res.status(200).json(campus);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "error finding table" });
+		next(error);
+	}
+});
 //post a campus  path api/campus/campus
-router.post("/", async (req, res, next)=>{
-    try{
-        const campus = await Campus.create(req.body);
-        res.status(200).json(campus);
-    }
-    catch(error){
-        console.error(error);
-        res.status(500).json({message: "unable to post campus"});
-        next(error);
-    }
-})
+router.post("/", async (req, res, next) => {
+	try {
+		const campus = await Campus.create(req.body);
+		res.status(200).json(campus);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "unable to post campus" });
+		next(error);
+	}
+});
 
-
+router.put("/:id", async (req, res, next) => {
+	try {
+		let campus = await Campus.findByPk(req.params.id);
+		if (!campus) res.status(404).json({ message: "Campus does not exist" });
+		campus.update({
+			name: req.body.name,
+			address: req.body.address,
+			imageUrl: req.body.imageUrl,
+			description: req.body.description,
+		});
+		campus.save();
+		res.status(200).json({
+			message: "Campus sucessfully updated",
+			campus,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "An error has occured while updating campus",
+		});
+		next(error);
+	}
+});
 
 module.exports = router;
