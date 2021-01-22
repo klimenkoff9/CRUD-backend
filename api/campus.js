6342323d1fc6d03b8525259012d10132c3d45d3e
Andrew Previl <drewprevil@gmail.com>
2021-01-22 10:19:59 -0500

    campus route change

--- api/campus.js
+++ api/campus.js
@@ -28,7 +28,7 @@ router.get("/:id", async(req, res, next)=>{
     }
 })
 //post a campus  path api/campus/campus
-router.post("/campus", async (req, res, next)=>{
+router.post("/", async (req, res, next)=>{
     try{
         const campus = await Campus.create(req.body);
         res.status(200).json(campus);
