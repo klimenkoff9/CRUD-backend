cda1a1a15e51f866a2a7500a85a7b042c9668756
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-23 11:17:45 -0500

    changes

--- api/index.js
+++ api/index.js
@@ -3,10 +3,9 @@ module.exports = router;
 
 router.use("/student", require("./student"));
 router.use("/campus", require("./campus"));
-// router.use('/trainers', require('./trainer'))
 
-// router.use((req, res, next) => {
-// 	const error = new Error("Not Found");
-// 	error.status = 404;
-// 	next(error);
-// });
+router.use((req, res, next) => {
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
+});
